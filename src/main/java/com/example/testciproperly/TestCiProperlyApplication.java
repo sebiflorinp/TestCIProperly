0127129ecad8324b3496ac9e55727c9b18eb0cbd
sebiflorinp <sebiflorinp@gmail.com>
2025-02-26 16:32:59 +0200

    First commit

---
+++ src/main/java/com/example/testciproperly/TestCiProperlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.testciproperly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCiProperlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCiProperlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testciproperly/TestCiProperlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testciproperly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCiProperlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
